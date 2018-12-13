c7f7290c1fc4fe9619ff8a0c64c621f52411550b
Anton Olin <anton.olin@neonode.com>
2018-12-13 15:27:07 +0100

    Started adding functionality to simplify the interface for the end user

--- Zforce/src/Zforce.cpp
+++ Zforce/src/Zforce.cpp
@@ -5,7 +5,7 @@ Zforce::Zforce()
 {
 }
 
-Zforce::Start(int dr)
+void Zforce::Start(int dr)
 {
   dataReady = dr;
   pinMode(dataReady, INPUT);
@@ -13,7 +13,7 @@ Zforce::Start(int dr)
   I2c.begin();
 }
 
-void Zforce::ReadMessage()
+int Zforce::Read(uint8_t * payload)
 {
   int status = 0;
   status = I2c.read(ZFORCE_I2C_ADDRESS, 2);
@@ -23,10 +23,9 @@ void Zforce::ReadMessage()
     return status;
   }
 
+  // Read the 2 I2C header bytes.
   payload[0] = I2c.receive();
   payload[1] = I2c.receive();
-
-  length = payload[1] + 1; // +1 in order to include the i2c header.
   
   status = I2c.read(ZFORCE_I2C_ADDRESS, payload[1], &payload[2]);
 
@@ -35,23 +34,145 @@ void Zforce::ReadMessage()
     return status;
   }
 
-  return status;
+  Enqueue(payload);
+
+  return status; // return 0 if success, otherwise error code according to Atmel Data Sheet
 }
 
 /*
  * Sends a message in the form of a byte array.
  */
-int Zforce::WriteMessage(uint8_t* data)
+int Zforce::Write(uint8_t* payload)
 {
-  int len = data[1] + 2;
-  int status = I2c.write(ZFORCE_I2C_ADDRESS, data[0], &data[1], len);
+  int len = payload[1] + 2;
+  int status = I2c.write(ZFORCE_I2C_ADDRESS, payload[0], &payload[1], len);
 
   return status; // return 0 if success, otherwise error code according to Atmel Data Sheet
+}
+
+bool Zforce::Enable(bool isEnabled)
+{
+  bool failed = false;
+
+  uint8_t enable[] = {0xEE, 0x0A, 0xEE, 0x08, 0x40, 0x02, 0x02, 0x00, 0x65, 0x02, (isEnabled ? 0x81 : 0x80), 0x00};
+  
+  if(GetDataReady() == HIGH)
+  {
+    Read(payload);
+  }
+
+  if (Write(enable))
+  {
+    failed = true;
+  }
+  else
+  {
+    lastSentMessage = enable;
+  }
+
+  long ms = millis();
+  while(GetDataReady() == LOW)
+  {
+    if((millis() - ms) > timeout)
+    {
+      failed = true;
+      break;
+    }
+  }
+
+  if(!Read(payload) && !failed)
+  {
+    failed = true;
+  }
+
+  if(!failed)
+  {
+    VirtualParse(payload);
+  }
+
+  return !failed;
+}
+
+Message Zforce::GetMessage()
+{
+  Message msg;
+  Dequeue(&msg);
+  return (Message)msg;
+}
+
+void Zforce::VirtualParse(uint8_t* payload)
+{
+  Message msg;
+  msg.type = payload[2];
+
+  switch(msg.type)
+  {
+    case RESPONSE:
+      switch(payload[8])
+      {
+        case 0x65: // It's an enable
+          Enable enable = (Enable)msg;
+          switch (payload[10])
+          {
+            case 0x80: // It's disabled
+              enable.enabled = false;
+            break;
+
+            case 0x81: // It's enabled
+              enable.enabled = true;
+            break;
+
+            default:
+            break;
+          }
+        break;
+        case 0x73: // It's a device configuration
+        break;
+        default:
+        break;
+      }
+      
+    break;
+
+    case NOTIFICATION:
+      if (payload[8] == 0xA0) // Check the identifier if this is a touch message or something else.
+      {
+        uint8_t touchCount = payload[9] / 11; // Calculate the amount of touch objects.
+        for (uint8_t i = 0; i < touchCount; i++)
+        {
+          Touch touch = (Touch)msg;
+
+          touch.id = payload[12 + (i * 11)];
+          touch.event = (TouchEvent)(payload[13 + (i * 11)];
+          touch.x = payload[14 + (i * 11)] << 8;
+          touch.x |= payload[15 + (i * 11)];
+          touch.y = payload[16 + (i * 11)] << 8;
+          touch.y |= payload[17 + (i * 11)];
+
+          Enqueue(msg);
+        }
+      }
+    break;
+
+    default:
+    break;
+  }
+
 }
 
 int Zforce::GetDataReady()
 {
   return digitalRead(dataReady);
+}
+
+void Zforce::Enqueue(Message msg)
+{
+  
+}
+
+void Zforce::Dequeue(Message* msg)
+{
+
 }
 
 /*
@@ -62,9 +183,5 @@ int Zforce::GetLength()
   return length;
 }
 
-uint8_t * Zforce::GetPayload()
-{
-  return payload;
-}
 
 Zforce zforce = Zforce();
--- Zforce/src/Zforce.h
+++ Zforce/src/Zforce.h
@@ -3,21 +3,102 @@
 #define MAX_PAYLOAD 255
 #define ZFORCE_I2C_ADDRESS 0x50
 
+enum TouchEvent
+{
+	DOWN = 0,
+	MOVE = 1,
+	UP = 2,
+	INVALID = 3,
+	GHOST = 4
+};
+
+enum MessageType
+{
+	RESPONSE = 0xEF,
+	NOTIFICATION = 0xF0
+};
+
+enum MessageIdentifier
+{
+	ENABLE,
+	TOUCHACTIVEAREA,
+	REVERSEX,
+	REVERSEY,
+	FLIPXY,
+	REPORTEDTOUCHES
+};
+
+struct Message
+{
+	MessageType type;
+};
+
+struct Touch : Message
+{
+	uint16_t x;
+	uint16_t y;
+	uint8_t id;
+	TouchEvent event;
+};
+
+struct Enable : Message
+{
+	bool enabled;
+};
+
+struct TouchActiveArea : Message
+{
+	uint16_t minX;
+	uint16_t minY;
+	uint16_t maxX;
+	uint16_t maxY;
+};
+
+struct FlipXY : Message
+{
+	bool flipXY;
+};
+
+struct ReverseX : Message
+{
+	bool reverseX;
+};
+
+struct ReverseY : Message
+{
+	bool reverseY;
+};
+
+struct ReportedTouches : Message
+{
+	uint8_t reportedTouches;
+};
+
 class Zforce 
 {
     public:
 		Zforce();
-		void ReadMessage();
-		int WriteMessage(uint8_t* data);
+		int Read(uint8_t* payload);
+		int Write(uint8_t* payload);
+		bool Enable(bool isEnabled);
+		bool TouchActiveArea(int minX, int minY, int maxX, int maxY); // Missing
+		bool FlipXY(bool isFlipped); // Missing
+		bool ReverseX(bool isRev); // Missing
+		bool ReverseY(bool isRev); // Missing
+		bool ReportedTouches(uint8_t reportedTouches); // Missing
+		void Start(int dr);
+		void Stop(); // Missing
+		Message GetMessage();
+    private:
 		int GetLength();
 		int GetDataReady();
-		int Start(int dr);
-		uint8_t * GetPayload();
-
-    private:
+		void VirtualParse(uint8_t* payload);
+		void Enqueue(Message msg);
+		void Dequeue(Message* msg);
 		uint8_t payload[MAX_PAYLOAD];
-		int length;
 		int dataReady;
+		const int timeout = 1000;
+		MessageIdentifier lastSentMessage;
 };
 
 extern Zforce zforce;
