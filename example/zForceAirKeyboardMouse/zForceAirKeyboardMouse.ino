7ac890d5d093f1622f09470fab3730ccef9249c8
Anton Olin <anton.olin@neonode.com>
2020-04-29 15:01:19 +0200

    Added comments about copyright notice and LGPL license, and also added a comment about what the demo actually does.

--- example/zForceAirKeyboardMouse/zForceAirKeyboardMouse.ino
+++ example/zForceAirKeyboardMouse/zForceAirKeyboardMouse.ino
@@ -1,3 +1,27 @@
+/*  Neonode zForce v7 interface library for Arduino
+
+    Copyright (C) 2020 Neonode Inc.
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/*  ------ Combined Keyboard and Relative Mouse Demo
+    This demo converts the touch data from the sensor to make it act as a relative mouse along with a few keyboard buttons.
+    The arduino is then recognized by the OS as a combined keyboard and relative mouse.
+*/
+
 #include <Keyboard.h>
 #include <Mouse.h>
 #include <Zforce.h>
