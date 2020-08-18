3f97d65d6ce0594c5636b67fe41a3aa89e268fc3
Josefin Johansson <josefin.johansson@neonode.com>
2020-08-18 16:43:59 +0200

    minor adjustments

--- example/zForceKeyboardMouse/zForceKeyboardMouse.ino
+++ example/zForceKeyboardMouse/zForceKeyboardMouse.ino
@@ -23,17 +23,19 @@
 
 #define DATA_READY PIN_NN_DR // change "PIN_NN_DR" to assigned i/o digital pin
 
-long globalMillis = millis();          // global timestamp
-const int keyboardBoundary = 1000;     // separate mouse area and keyboard area on the x-axis by 1000 (1/10)mm
-const int holdTime = 80;               // sensitivity for mouse "left click", unit in milli-second
+long globalMillis = millis();         // global timestamp
+const int keyboardBoundary = 750;     // separate mouse area and keyboard area on the x-axis by 75 mm.
+const int holdTime = 200;             // sensitivity for mouse "left-click", unit in milli-second
 
-typedef struct Location
+typedef struct TouchInformaiton
 {
   uint16_t x;
   uint16_t y;
-} Location;
+  uint8_t event;
+} TouchInformation;
 
-Location previousLoc;
+TouchInformaiton currentTouch;
+TouchInformation previousTouch;
 
 void setup()
 {
@@ -52,65 +54,6 @@ void setup()
     Serial.println((int)msg->type);
     zforce.DestroyMessage(msg);
   }
-
-  // Send and read ReverseX
-  zforce.ReverseX(false);
-
-  do
-  {
-    msg = zforce.GetMessage();
-  } while (msg == NULL);
-
-  if (msg->type == MessageType::REVERSEXTYPE)
-  {
-    Serial.println("Received ReverseX Response");
-    Serial.print("Message type is: ");
-    Serial.println((int)msg->type);
-  }
-
-  zforce.DestroyMessage(msg);
-
-
-  // Send and read ReverseY
-  zforce.ReverseY(false);
-
-  do
-  {
-    msg = zforce.GetMessage();
-  } while (msg == NULL);
-
-  if (msg->type == MessageType::REVERSEYTYPE)
-  {
-    Serial.println("Received ReverseY Response");
-    Serial.print("Message type is: ");
-    Serial.println((int)msg->type);
-  }
-
-  zforce.DestroyMessage(msg);
-
-  // Send and read Touch Active Area
-  zforce.TouchActiveArea(0, 0, 4000, 4000);
-
-  do
-  {
-    msg = zforce.GetMessage();
-  } while (msg == NULL);
-
-  if (msg->type == MessageType::TOUCHACTIVEAREATYPE)
-  {
-    Serial.print("minX is: ");
-    Serial.println(((TouchActiveAreaMessage*)msg)->minX);
-    Serial.print("minY is: ");
-    Serial.println(((TouchActiveAreaMessage*)msg)->minY);
-    Serial.print("maxX is: ");
-    Serial.println(((TouchActiveAreaMessage*)msg)->maxX);
-    Serial.print("maxY is: ");
-    Serial.println(((TouchActiveAreaMessage*)msg)->maxY);
-  }
-
-  zforce.DestroyMessage(msg);
-
-  // Send and read Enable
 
   zforce.Enable(true);
 
@@ -139,32 +82,67 @@ void loop()
   {
     if (touch->type == MessageType::TOUCHTYPE)
     {
-      uint16_t x = ((TouchMessage*)touch)->touchData[0].x;
-      uint16_t y = ((TouchMessage*)touch)->touchData[0].y;
-      uint8_t event = ((TouchMessage*)touch)->touchData[0].event;
+      currentTouch.x = ((TouchMessage*)touch)->touchData[0].x;
+      currentTouch.y = ((TouchMessage*)touch)->touchData[0].y;
+      currentTouch.event = ((TouchMessage*)touch)->touchData[0].event;
 
-      loopMouse(x, y, event);
-      loopKeyboard(x, y, event);
+      loopMouse();
+      loopKeyboard();
     }
     zforce.DestroyMessage(touch);
   }
 }
+DBC
+void loopMouse() {
+  if (currentTouch.x <= keyboardBoundary)
+    return;  //return if the touch object is within the keyboard area
 
-void loopKeyboard(uint16_t x, uint16_t y, uint8_t event) {
-  if (x > keyboardBoundary)// outside of keyboard area, do nothing
-    return;
-    
+  switch (currentTouch.event)
+  {
+    case 0:  // DOWN event
+      previousTouch.x =  currentTouch.x;
+      previousTouch.y =  currentTouch.y;
+      globalMillis = millis();
+      Serial.println("Mouse Input - DOWN");
+      break;
+
+    case 1: // MOVE event
+      if ((millis() - globalMillis) >= holdTime)
+      {
+        Mouse.move((currentTouch.x - previousTouch.x), (currentTouch.y - previousTouch.y));
+        Serial.println("Mouse Input - Moving cursor");
+      }
+      previousTouch.x = currentTouch.x;
+      previousTouch.y = currentTouch.y;
+      break;
+
+    case 2: // UP event
+      Serial.print("Mouse Input - UP");
+      if (millis() - globalMillis < holdTime)  // mouse "left click", sensitivity
+      { // can be tuned by changing "holdTime"
+        Mouse.click(MOUSE_LEFT);
+        Serial.print("(Left-Click)");
+      }
+      Serial.println("");
+      break;
+  }
+}
+
+void loopKeyboard() {
+  if (currentTouch.x > keyboardBoundary)
+    return; //return if the touch object is within the mouse pad area
+
   char key;
-  if (event == 0) { // Down
+  if (currentTouch.event == 0) { // Down
 
     //assign Key to the given interval
-    if (y < 250)
+    if (currentTouch.y < 250)
       key = 'A';
-    else if (y < 500)
+    else if (currentTouch.y < 500)
       key = 'B';
-    else if (y < 750)
+    else if (currentTouch.y < 750)
       key = 'C';
-    else if (y < 1000)
+    else if (currentTouch.y < 1000)
       key = 'D';
     else
       key = 'E';
@@ -176,39 +154,5 @@ void loopKeyboard(uint16_t x, uint16_t y, uint8_t event) {
   else
   {
     // May do something to catch the rest of the cases
-  }
-}
-
-void loopMouse(uint16_t x, uint16_t y, uint8_t event) {
-  if (x <= keyboardBoundary)   // outside mouse area, do nothing
-    return;
-
-  Serial.print("Mouse Input - ");
-  if (event == 0) // touch "Down" event
-  {
-    previousLoc.x =  x;
-    previousLoc.y =  y;
-    globalMillis = millis();
-    Serial.println(" DOWN");
-  }
-  else if (event == 1) // touch "Move" event
-  {
-    if (x >= keyboardBoundary)  // within mouse area
-    {
-      if ((millis() - globalMillis) >= holdTime)
-        Mouse.move((x - previousLoc.x), (y - previousLoc.y));
-      previousLoc.x = x;
-      previousLoc.y = y;
-      Serial.println("Move cursor");
-    }
-  }
-  else if (event == 2)  // touch "Up" event
-  {
-    Mouse.click(MOUSE_LEFT);
-    if (millis() - globalMillis < holdTime)  // mouse "left click", sensitivity
-    { // can be tuned by changing "holdTime"
-      Mouse.click(MOUSE_LEFT);
-      Serial.println("UP left click");
-    }
   }
 }
